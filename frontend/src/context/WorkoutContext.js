3594c789a0ea526af5290d482363ea7a94d38af8
faiz-webdev <faijuddin@tothenew.com>
2023-07-04 15:55:53 +0530

    2- creating context api

---
+++ frontend/src/context/WorkoutContext.js
@@ -0,0 +1,12 @@
+import { createContext } from "react";
+
+const WorkoutContext = createContext();
+
+export const WorkoutsContextProvider = () => {
+
+    return (
+        <WorkoutContext.Provider>
+
+        </WorkoutContext.Provider>
+    )
+}
