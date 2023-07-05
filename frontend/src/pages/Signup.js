211212611cee8e4f4d20822c66afca1202f49d36
faiz-webdev <faijuddin@tothenew.com>
2023-07-05 14:02:13 +0530

    14- making a useSignup hook

--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -1,26 +1,38 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
+import { useSignup } from "../components/hooks/useSignup";
 
 function Signup() {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { signup, error, isLoading } = useSignup();
 
-    const handleSubmit = async(e)=> {
-        e.preventDefault();
-        console.log(email, password);
-    }
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    await signup(email, password);
 
+    console.log(email, password);
+  };
 
   return (
-    <form className='signup' onSubmit={handleSubmit} noValidate>
-        <h3>Sign up</h3>
-        <label htmlFor="">Email:</label>
-        <input type="email" onChange={(e)=> setEmail(e.target.value)} value={email} />
-        <label htmlFor="">Password:</label>
-        <input type="password" onChange={(e)=> setPassword(e.target.value)} value={password} />
+    <form className="signup" onSubmit={handleSubmit} noValidate>
+      <h3>Sign up</h3>
+      <label htmlFor="">Email:</label>
+      <input
+        type="email"
+        onChange={(e) => setEmail(e.target.value)}
+        value={email}
+      />
+      <label htmlFor="">Password:</label>
+      <input
+        type="password"
+        onChange={(e) => setPassword(e.target.value)}
+        value={password}
+      />
 
-        <button>Signup</button>
+      <button disabled={isLoading}>Signup</button>
+      {error && <div className="error">{error}</div>}
     </form>
-  )
+  );
 }
 
-export default Signup
+export default Signup;
