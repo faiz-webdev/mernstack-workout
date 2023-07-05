8df94339fec8cc83298dfe00e6f24afe624877a2
faiz-webdev <faijuddin@tothenew.com>
2023-07-05 11:02:49 +0530

    8- signup user completed

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -7,7 +7,15 @@ const loginUser = async (req, res) => {
 
 //signup user
 const signupUser = async (req, res) => {
-  res.json({ msg: "signup success" });
+  try {
+    const {email, password} = req.body;
+    const user = await User.signup(email, password);
+
+    res.status(200).json({email, user});
+    
+  } catch (error) {
+    res.status(400).json({error: error.message});
+  }
 };
 
 module.exports = { loginUser, signupUser };
