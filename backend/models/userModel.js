586b8644ff8080c0ef93a26ae15906ac89ef020a
faiz-webdev <faijuddin@tothenew.com>
2023-07-05 11:09:04 +0530

    9- email and password validation

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
+const validator = require("validator");
 
 const Schema = mongoose.Schema;
 
@@ -19,7 +20,20 @@ const userSchema = new Schema(
 );
 
 //static signup method
-userSchema.statics.signup = async function(email, password) {
+userSchema.statics.signup = async function (email, password) {
+  //validation
+  if (!email || !password) {
+    throw Error("All fields must be filled");
+  }
+
+  if (!validator.isEmail(email)) {
+    throw Error("Email must be a valid email");
+  }
+
+  if (!validator.isStrongPassword(password)) {
+    throw Error("Password not strong enough");
+  }
+
   const exists = await this.findOne({ email });
 
   if (exists) {
